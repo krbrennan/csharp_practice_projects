31cdcde1a56da441efb1e4f7facf3a46cf4ddb60
Kevin Brennan <krb.brennan@gmail.com>
2025-03-15 14:58:35 -0400

    adding local files to remote repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# personal_cSharp
+
+please
---
+++ Sorting and Searching Arrays/Sorting and Searching Arrays/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+var names = new List<string> { "Bob", "Cindy", "Zach", "Nathan", "Abel"};
+var firstSort = new SortingNames(names);
+Console.WriteLine($"Sorting names: {names}");
+//firstSort
+Console.ReadLine();
+
+
+
+class SortingNames
+{
+    private List<string> _names { get; set; }
+    public SortingNames(List<string> names) {
+        _names = names;
+    }
+
+
+    public List<string> ReadNames
+    {
+        get { return _names; }
+    }
+
+    public string AddNames(string[] names)
+    {
+        if(names.Length > 0)
+        {
+            _names.Add(names);
+            return $"Names added: {names}";
+        }
+    }
+}
+
+
+
+
+
